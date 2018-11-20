bf847897dd97626b8071811e1eab73aeee0e8535
microhhh <13654840887@163.com>
2018-11-20 20:08:15 +0800

    translator

--- translator/metrics/accuracy.py
+++ translator/metrics/accuracy.py
@@ -1,15 +1,28 @@
 val = open('val.txt', encoding='UTF-8')
 out = open('out.txt', encoding='UTF-8')
 
-t = val.read()
-a = out.read()
+t = val.readline()
+a = out.readline()
 sum = 0
 right = 0
-for i in range(len(t)):
-    sum += 1
-    if t[i] == '\n':
-        continue
-    if t[i] == a[i]:
-        right += 1
 
+while t and a:
+    error = []
+    t = t.strip()
+    a = a.strip()
+    for i in range(len(t)):
+        sum += 1
+        if t[i] == a[i]:
+            right += 1
+        else:
+            error.append(a[i])
+    print(t)
+    print(a)
+    print("wrong with {}", format(error))
+
+    t = val.readline()
+    a = out.readline()
+print("right={}".format(right))
+print("sum={}".format(sum))
 print(right / sum)
+
